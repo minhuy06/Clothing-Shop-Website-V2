662ec17394be090d4249eb23d4b4f0243e7d3bc6
Võ Minh Huy <1410huyvo@gmail.com>
2026-05-07 18:19:17 +0700

    Add project files.

---
+++ Clothing Shop Website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Clothing_Shop_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

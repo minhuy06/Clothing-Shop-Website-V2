3946c7c437bb0bd2d710e610be71dc37bdf86969
Khanhvo48 <khanhvo644@gmail.com>
2026-05-10 21:46:48 +0700

    Add files via upload

---
+++ Clothing Shop Website/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class AdminController : Controller
+{
+    public IActionResult Dashboard()
+    {
+        return View();
+    }
+
+    public IActionResult Products()
+    {
+        return View();
+    }
+
+    public IActionResult Orders()   
+    {
+        return View();
+    }
+}
